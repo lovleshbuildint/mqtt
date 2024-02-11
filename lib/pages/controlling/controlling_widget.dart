b1075fa9a2172eedcbc580201ce7b10caab0a087
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-11 17:39:58 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/controlling/controlling_widget.dart
+++ lib/pages/controlling/controlling_widget.dart
@@ -55,66 +55,6 @@ class _ControllingWidgetState extends State<ControllingWidget> {
             );
           }(),
         );
-        unawaited(
-          () async {
-            await actions.publishMqtt(
-              context,
-              'Settings',
-              '${widget.did}\$GRES,',
-              FFAppState().deviceId,
-            );
-          }(),
-        );
-        if ((FFAppState().deviceStateDid != null &&
-                FFAppState().deviceStateDid != '') &&
-            (FFAppState().deviceStateDid != _model.maskStatus)) {
-          setState(() {
-            _model.ac1Value = ((String var1) {
-              return var1.split(',')[0][7] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.ac2Value = ((String var1) {
-              return var1.split(',')[0][7] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.lobbyLightValue = ((String var1) {
-              return var1.split(',')[0][2] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.signageValue = ((String var1) {
-              return var1.split(',')[0][3] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.routerValue = ((String var1) {
-              return var1.split(',')[0][4] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.vsatValue = ((String var1) {
-              return var1.split(',')[0][5] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.dvrValue = ((String var1) {
-              return var1.split(',')[0][6] == "1";
-            }(FFAppState().deviceStateDid));
-          });
-          setState(() {
-            _model.relayStatus = functions.decimalToBinary((String var1) {
-              return int.parse(var1.split(',').last);
-            }(FFAppState().deviceStateDid));
-            _model.maskStatus = FFAppState().deviceStateDid;
-          });
-        } else {
-          setState(() {
-            _model.relayStatus = null;
-          });
-        }
-
         _model.instantTimer = InstantTimer.periodic(
           duration: Duration(milliseconds: 5000),
           callback: (timer) async {
@@ -128,8 +68,9 @@ class _ControllingWidgetState extends State<ControllingWidget> {
                 );
               }(),
             );
-            if (FFAppState().deviceStateDid != null &&
-                FFAppState().deviceStateDid != '') {
+            if ((FFAppState().deviceStateDid != null &&
+                    FFAppState().deviceStateDid != '') &&
+                (FFAppState().deviceStateDid != _model.maskStatus)) {
               setState(() {
                 _model.ac1Value = ((String var1) {
                   return var1.split(',')[0][7] == "1";
@@ -170,10 +111,6 @@ class _ControllingWidgetState extends State<ControllingWidget> {
                   return int.parse(var1.split(',').last);
                 }(FFAppState().deviceStateDid));
                 _model.maskStatus = FFAppState().deviceStateDid;
-              });
-            } else {
-              setState(() {
-                _model.relayStatus = null;
               });
             }
           },
