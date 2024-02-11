465d8dcb75ec45330c56fdf661572770b2a22e73
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-11 14:08:22 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/controlling/controlling_widget.dart
+++ lib/pages/controlling/controlling_widget.dart
@@ -633,29 +633,46 @@ class _ControllingWidgetState extends State<ControllingWidget> {
                                       decoration: BoxDecoration(
                                         color: Color(0x00FFFFFF),
                                       ),
-                                      alignment: AlignmentDirectional(0.0, 0.0),
-                                      child: Transform.scale(
-                                        scaleX: 0.7,
-                                        scaleY: 0.7,
-                                        child: Switch.adaptive(
-                                          value: _model.switchValue ??= true,
-                                          onChanged: (newValue) async {
-                                            setState(() =>
-                                                _model.switchValue = newValue!);
-                                          },
-                                          activeColor:
-                                              FlutterFlowTheme.of(context)
-                                                  .primary,
-                                          activeTrackColor:
-                                              FlutterFlowTheme.of(context)
-                                                  .accent1,
-                                          inactiveTrackColor:
-                                              FlutterFlowTheme.of(context)
-                                                  .alternate,
-                                          inactiveThumbColor:
-                                              FlutterFlowTheme.of(context)
-                                                  .secondaryText,
-                                        ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Manual',
+                                            style: FlutterFlowTheme.of(context)
+                                                .bodyMedium
+                                                .override(
+                                                  fontFamily: 'Readex Pro',
+                                                  color: Color(0xFF929395),
+                                                  fontSize: 10.0,
+                                                ),
+                                          ),
+                                          Transform.scale(
+                                            scaleX: 0.7,
+                                            scaleY: 0.7,
+                                            child: Switch.adaptive(
+                                              value: _model.switchValue ??=
+                                                  true,
+                                              onChanged: (newValue) async {
+                                                setState(() => _model
+                                                    .switchValue = newValue!);
+                                              },
+                                              activeColor:
+                                                  FlutterFlowTheme.of(context)
+                                                      .primary,
+                                              activeTrackColor:
+                                                  FlutterFlowTheme.of(context)
+                                                      .accent1,
+                                              inactiveTrackColor:
+                                                  FlutterFlowTheme.of(context)
+                                                      .alternate,
+                                              inactiveThumbColor:
+                                                  FlutterFlowTheme.of(context)
+                                                      .secondaryText,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ],
