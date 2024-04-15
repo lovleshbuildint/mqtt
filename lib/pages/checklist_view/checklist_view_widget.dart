ef9848f3a9e2a93610453b768fd2d494fe0b1f4f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-15 07:28:21 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/checklist_view/checklist_view_widget.dart
+++ lib/pages/checklist_view/checklist_view_widget.dart
@@ -369,6 +369,16 @@ class _ChecklistViewWidgetState extends State<ChecklistViewWidget> {
                         ),
                       ),
                     ),
+                    Expanded(
+                      child: Text(
+                        checklistViewGetChecklistViewResponse.jsonBody
+                            .toString(),
+                        style: FlutterFlowTheme.of(context).bodyMedium.override(
+                              fontFamily: 'Readex Pro',
+                              letterSpacing: 0.0,
+                            ),
+                      ),
+                    ),
                   ],
                 ),
               ),
