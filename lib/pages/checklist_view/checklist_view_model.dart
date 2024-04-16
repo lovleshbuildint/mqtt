91fee4730f2a1f6ef329fdb19267111fea8fd244
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-16 19:03:37 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/checklist_view/checklist_view_model.dart
+++ lib/pages/checklist_view/checklist_view_model.dart
@@ -3,20 +3,30 @@ import '/flutter_flow/flutter_flow_data_table.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'checklist_view_widget.dart' show ChecklistViewWidget;
 import 'dart:async';
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class ChecklistViewModel extends FlutterFlowModel<ChecklistViewWidget> {
+  ///  Local state fields for this page.
+
+  String? searchValue;
+
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
   // Stores action output result for [Backend Call - API (User Info)] action in ChecklistView widget.
   ApiCallResponse? userInfoRespnse;
   Completer<ApiCallResponse>? apiRequestCompleter;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode;
+  TextEditingController? textController;
+  String? Function(BuildContext, String?)? textControllerValidator;
   // State field(s) for PaginatedDataTable widget.
   final paginatedDataTableController =
       FlutterFlowDataTableController<dynamic>();
@@ -29,6 +39,8 @@ class ChecklistViewModel extends FlutterFlowModel<ChecklistViewWidget> {
   @override
   void dispose() {
     unfocusNode.dispose();
+    textFieldFocusNode?.dispose();
+    textController?.dispose();
   }
 
   /// Additional helper methods.
