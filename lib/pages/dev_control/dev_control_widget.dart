2cb2437a0621a7a1d8a03a3bff71ad97b502af55
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-26 15:36:00 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dev_control/dev_control_widget.dart
+++ lib/pages/dev_control/dev_control_widget.dart
@@ -74,9 +74,9 @@ class _DevControlWidgetState extends State<DevControlWidget> {
           }(FFAppState().mqttResponse)) {
             setState(() {
               _model.devResponse = FFAppState().mqttResponse;
-              _model.setTimeDay = ((String? var1) {
-                return var1?.split(',')[4];
-              }(_model.devResponse))!;
+              _model.setTimeDay = (String var1) {
+                return var1.split(',')[4];
+              }(_model.devResponse!);
             });
             return;
           } else {
