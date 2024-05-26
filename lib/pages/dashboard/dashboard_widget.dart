f551ea0fe6c758003da2d7f2374c9070197b71aa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-26 11:28:49 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dashboard/dashboard_widget.dart
+++ lib/pages/dashboard/dashboard_widget.dart
@@ -201,7 +201,6 @@ class _DashboardWidgetState extends State<DashboardWidget> {
                                           await showModalBottomSheet(
                                             isScrollControlled: true,
                                             backgroundColor: Colors.transparent,
-                                            enableDrag: false,
                                             useSafeArea: true,
                                             context: context,
                                             builder: (context) {
