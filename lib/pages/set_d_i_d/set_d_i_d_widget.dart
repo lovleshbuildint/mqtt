6a2db4f9a269536ed635b193eff5f330de000d7f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-26 10:54:19 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/set_d_i_d/set_d_i_d_widget.dart
+++ lib/pages/set_d_i_d/set_d_i_d_widget.dart
@@ -342,11 +342,9 @@ class _SetDIDWidgetState extends State<SetDIDWidget>
                         child: Text(
                           '${FFAppState().mqttResponse} ${_model.maxTry?.toString()} ${'${(String var1) {
                             return var1.split(',')[0] +
-                                ',' +
                                 var1.split(',')[2] +
-                                ',' +
                                 var1.split(',')[3];
-                          }(FFAppState().mqttResponse)}'}->${'${widget.deviceMacId},\$SDID,OK'}',
+                          }(FFAppState().mqttResponse)}'}->${'${widget.deviceMacId}\$SDIDOK'}',
                           style:
                               FlutterFlowTheme.of(context).bodyMedium.override(
                                     fontFamily: 'Poppins',
