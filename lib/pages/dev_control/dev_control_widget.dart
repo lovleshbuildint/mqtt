1f8a8ebe08d9591a230b2b7256d36ef5d1d17594
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-26 15:22:29 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dev_control/dev_control_widget.dart
+++ lib/pages/dev_control/dev_control_widget.dart
@@ -447,7 +447,8 @@ class _DevControlWidgetState extends State<DevControlWidget> {
                                       ),
                                     ),
                                     if ((String? var1) {
-                                      return var1?.split(',')[4] != null
+                                      return var1?.split(',')[4] != null ||
+                                              var1?.split(',')[4] != ''
                                           ? true
                                           : false;
                                     }(_model.devResponse))
