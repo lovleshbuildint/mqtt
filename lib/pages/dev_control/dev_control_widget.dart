d0b34d8fb8353b0b599ce312af76df9c32787986
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-27 05:43:09 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dev_control/dev_control_widget.dart
+++ lib/pages/dev_control/dev_control_widget.dart
@@ -146,6 +146,7 @@ class _DevControlWidgetState extends State<DevControlWidget>
               return var1.split(',')[13];
             }(_model.devResponse));
           });
+          break;
         } else if ((_model.maxTry > 14) &&
             ((String? var1) {
               return var1?.split(',')[2] != '\$GDEV' ? true : false;
