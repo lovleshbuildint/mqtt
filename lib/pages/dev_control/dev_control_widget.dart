17a3e0a71a59f880a60f7cba85f3d94d4d401d2d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-27 05:50:11 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dev_control/dev_control_widget.dart
+++ lib/pages/dev_control/dev_control_widget.dart
@@ -4,10 +4,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
-import 'dart:async';
 import 'dart:math';
-import '/actions/actions.dart' as action_blocks;
-import '/custom_code/actions/index.dart' as actions;
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -42,115 +39,6 @@ class _DevControlWidgetState extends State<DevControlWidget>
   void initState() {
     super.initState();
     _model = createModel(context, () => DevControlModel());
-
-    // On page load action.
-    SchedulerBinding.instance.addPostFrameCallback((_) async {
-      await action_blocks.userInfoUpdate(context);
-      setState(() {});
-      unawaited(
-        () async {
-          await actions.subscribeMqtt(
-            context,
-            'Response',
-            FFAppState().deviceId,
-            widget.did,
-            '15.206.230.32',
-            'mqtt_buildint_\$\$2023',
-          );
-        }(),
-      );
-      while (_model.maxTry < 16) {
-        if (((String? var1) {
-              return var1?.split(',')[2] == '\$GDEV' ? true : false;
-            }(FFAppState().mqttResponse)) &&
-            (_model.maxTry < 15)) {
-          setState(() {
-            _model.devResponse = FFAppState().mqttResponse;
-          });
-          setState(() {
-            _model.dropDownValueController?.value = ((String var1) {
-              return var1.split(',')[3];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.setTimeDayTextController?.text = ((String var1) {
-              return var1.split(',')[4];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.setTimeNightTextController?.text = ((String var1) {
-              return var1.split(',')[5];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.signageOnTimeTextController?.text = ((String var1) {
-              return var1.split(',')[6];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.signageOffTimeTextController?.text = ((String var1) {
-              return var1.split(',')[7];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.dataIntervalTextController?.text = ((String var1) {
-              return var1.split(',')[8];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.maxTempDayTextController?.text = ((String var1) {
-              return var1.split(',')[9];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.minTempDayTextController?.text = ((String var1) {
-              return var1.split(',')[10];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.maxTempNightTextController?.text = ((String var1) {
-              return var1.split(',')[11];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.minTempNightTextController?.text = ((String var1) {
-              return var1.split(',')[12];
-            }(_model.devResponse));
-          });
-          setState(() {
-            _model.acOnTimeDayTextController?.text = ((String var1) {
-              return var1.split(',')[13];
-            }(_model.devResponse));
-          });
-          break;
-        } else if ((_model.maxTry > 14) &&
-            ((String? var1) {
-              return var1?.split(',')[2] != '\$GDEV' ? true : false;
-            }(FFAppState().mqttResponse))) {
-          setState(() {
-            _model.noResponse = true;
-          });
-          break;
-        } else {
-          setState(() {
-            _model.maxTry = _model.maxTry + 1;
-          });
-          await Future.delayed(const Duration(milliseconds: 1000));
-          unawaited(
-            () async {
-              await actions.publishMqtt(
-                context,
-                'Settings',
-                '${widget.did}\$GDEV,',
-                FFAppState().deviceId,
-                '15.206.230.32',
-                'mqtt_buildint_\$\$2023',
-              );
-            }(),
-          );
-        }
-      }
-    });
 
     _model.setTimeDayTextController ??= TextEditingController();
     _model.setTimeDayFocusNode ??= FocusNode();
@@ -2173,6 +2061,37 @@ class _DevControlWidgetState extends State<DevControlWidget>
                             ),
                           ),
                         ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            50.0, 0.0, 50.0, 18.0),
+                        child: FFButtonWidget(
+                          onPressed: () {
+                            print('Button pressed ...');
+                          },
+                          text: 'GET DEV',
+                          options: FFButtonOptions(
+                            height: 40.0,
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                24.0, 0.0, 24.0, 0.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: FlutterFlowTheme.of(context).primary,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0.0,
+                                ),
+                            elevation: 3.0,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   if (_model.noResponse)
