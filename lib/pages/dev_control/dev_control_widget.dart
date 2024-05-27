01d3f44ccc89cf910edbee3896729cf689242b95
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-27 06:17:52 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/dev_control/dev_control_widget.dart
+++ lib/pages/dev_control/dev_control_widget.dart
@@ -2239,6 +2239,44 @@ class _DevControlWidgetState extends State<DevControlWidget>
                             ),
                           ),
                         ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            50.0, 0.0, 50.0, 18.0),
+                        child: FFButtonWidget(
+                          onPressed: () async {
+                            setState(() {
+                              _model.maxTry = 0;
+                            });
+                            while (_model.maxTry < 16) {
+                              setState(() {
+                                _model.maxTry = _model.maxTry + 1;
+                              });
+                            }
+                          },
+                          text: 'GET DEV',
+                          options: FFButtonOptions(
+                            height: 40.0,
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                24.0, 0.0, 24.0, 0.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: FlutterFlowTheme.of(context).primary,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0.0,
+                                ),
+                            elevation: 3.0,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   if (_model.noResponse)
