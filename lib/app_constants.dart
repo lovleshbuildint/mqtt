07ba7d9125d78c60c471103e58a9d8f287591a79
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-12 11:53:38 +0000

    Updating to latest FlutterFlow output.

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 
 abstract class FFAppConstants {
-  static const String appVersion = '1.0.9';
+  static const String appVersion = '1.1.0';
 }
