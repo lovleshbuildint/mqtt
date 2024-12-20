df972b7194769564507b5588bd9187fe7ac11986
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-20 11:14:04 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/publish_mqtt.dart
+++ lib/custom_code/actions/publish_mqtt.dart
@@ -11,14 +11,10 @@ import 'package:flutter/material.dart';
 
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
-import 'package:mqtt_client/mqtt_browser_client.dart';
-import 'package:flutter/foundation.dart'; // For kIsWeb
 
 Future<String> publishMqtt(BuildContext context, String? pubtopic,
     String? message, String? deviceId, String ip, String pass) async {
-  final client = kIsWeb
-      ? MqttBrowserClient('wss://$ip', '') // Use WebSocket for web
-      : MqttServerClient(ip, '');
+  final MqttServerClient client = MqttServerClient(ip, '');
 
   final MqttConnectMessage connectMessage = MqttConnectMessage()
       .withClientIdentifier('$deviceId-publish')
